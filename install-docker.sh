2bd3d2413875700325e814be32b5144ac7b26774
Zemichael Tesfaye <zzimick@gmail.com>
2021-05-05 08:02:21 +0300

    add user to sudo group

--- install-docker.sh
+++ install-docker.sh
@@ -8,3 +8,4 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo gpasswd -a $USER docker
