e5b520ea903bfc6dda27aa70d1a623984c5ad55a
Zemichael Tesfaye <zzimick@gmail.com>
2022-12-15 08:19:11 +0300

    feat: added docker install for new version

---
+++ install-docker-new.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# remove installed
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# update repo
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release
+
+# add docker GPG
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# setup repo
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt-get update
+
+# install engine
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# post install
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chmod g+rwx "$HOME/.docker" -R
