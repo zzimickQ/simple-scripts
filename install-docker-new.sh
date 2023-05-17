de808aac09e2f6f84941ea2dfdda3ab518d79a5e
Zemichael Tesfaye <zzimick@gmail.com>
2023-05-17 08:41:47 +0300

    feat: upgrade docker installation script

--- install-docker-new.sh
+++ install-docker-new.sh
@@ -5,27 +5,32 @@ sudo apt-get remove docker docker-engine docker.io containerd runc
 
 # update repo
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo apt-get install ca-certificates curl gnupg
+
 
 # add docker GPG
-sudo mkdir -p /etc/apt/keyrings
+sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # setup repo
 echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# update repo
 sudo apt-get update
 
 # install engine
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # post install
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
+
+# setup configuration path
+sudo mkdir -p /home/"$USER"/.docker "$HOME/.docker"
 sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
 sudo chmod g+rwx "$HOME/.docker" -R
